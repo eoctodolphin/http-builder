a5551d84856e83a2d91e090ba9ed1c7a223a55e8
Erik Olsson <erikolsson027@gmail.com>
2022-03-11 23:54:07 +0100

    wip

--- src/test/java/io/eoctodolphin/client/ClientBuilderTest.java
+++ src/test/java/io/eoctodolphin/client/ClientBuilderTest.java
@@ -1,18 +1,7 @@
 package io.eoctodolphin.client;
-import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import okhttp3.mockwebserver.MockResponse;
-import okhttp3.mockwebserver.MockWebServer;
-import okhttp3.mockwebserver.RecordedRequest;
-
-import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ClientBuilderTest 
